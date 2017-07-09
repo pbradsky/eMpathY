8568aed15413e0ad4360e6a4170144c3ad4b5807
pbradsky <paytonbradsky@gmail.com>
2017-07-08 21:33:43 -0700

    take 2

--- MPathY/DataViewController.swift
+++ MPathY/DataViewController.swift
@@ -17,6 +17,7 @@ class DataViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("sup")
     }
 
     override func didReceiveMemoryWarning() {
